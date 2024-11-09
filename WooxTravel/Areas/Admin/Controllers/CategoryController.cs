f22e7a7706655eee8155cc29b21b8e19f836d043
mgurkankaya <https://github.com/mgurkankaya>
2024-11-09 21:51:16 +0300

    Finish

--- WooxTravel/Areas/Admin/Controllers/CategoryController.cs
+++ WooxTravel/Areas/Admin/Controllers/CategoryController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using WooxTravel.Context;
+using WooxTravel.Entities;
 
 namespace WooxTravel.Areas.Admin.Controllers
 {
@@ -18,10 +19,38 @@ namespace WooxTravel.Areas.Admin.Controllers
             return View(values);
         }
 
+        public ActionResult CreateCategory()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CreateCategory(Category category)
+        {
+            var values = context.Categories.Add(category);
+            context.SaveChanges();
+            return RedirectToAction("CategoryList", "Category", "Admin");
+        }
+
         public ActionResult DeleteCategory(int id)
         {
             var value = context.Categories.Find(id);
             context.Categories.Remove(value);
+            context.SaveChanges();
+            return RedirectToAction("CategoryList", "Category", "Admin");
+        }
+
+
+        public ActionResult UpdateCategory(int id)
+        {
+            var value = context.Categories.Find(id);
+            return View(value);
+        }
+        [HttpPost]
+        public ActionResult UpdateCategory(Category category)
+        {
+            var value = context.Categories.Find(category.CategoryId);
+            value.Name = category.Name;
+            category.CategoryStatus = true;
             context.SaveChanges();
             return RedirectToAction("CategoryList", "Category", "Admin");
         }
---
+++ WooxTravel/Areas/Admin/Controllers/StaticticsController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WooxTravel.Context;
+
+namespace WooxTravel.Areas.Admin.Controllers
+{
+    public class StatisticsController : Controller
+    {
+        private readonly TravelContext _context = new TravelContext(); // Veri tabanı bağlamı
+
+        public ActionResult Index()
+        {
+            // 1. Rotalar grafiği için veri (Ülkelere göre varış noktaları sayısı)
+            var destinationsData = _context.Destinations.GroupBy(d => d.Country)
+                                             .Select(g => new
+                                             {
+                                                 Country = g.Key,
+                                                 Count = g.Count()
+                                             }).ToList();
+
+            // 2. Mesajlar grafiği için veri (Okunmuş ve okunmamış mesaj sayısı)
+            var messagesData = _context.Messages.GroupBy(m => m.IsRead)
+                                                .Select(g => new
+                                                {
+                                                    Status = g.Key ? "Okundu" : "Okunmadı",
+                                                    Count = g.Count()
+                                                }).ToList();
+
+            // 3. Kategoriler grafiği için veri (Kategori durumuna göre kategori sayısı)
+            var categoriesData = _context.Categories.GroupBy(c => c.CategoryStatus)
+                                                    .Select(g => new
+                                                    {
+                                                        Status = g.Key,
+                                                        Count = g.Count()
+                                                    }).ToList();
+
+            // 4. Rezervasyonlar grafiği için veri (Günlük rezervasyon sayısı)
+            var reservationsData = _context.Reservations.GroupBy(r => r.ReservationDate)
+                                                        .Select(g => new
+                                                        {
+                                                            Date = g.Key,
+                                                            Count = g.Count()
+                                                        }).ToList();
+
+            // Veriyi View'e gönder
+            ViewBag.DestinationsData = destinationsData;
+            ViewBag.MessagesData = messagesData;
+            ViewBag.CategoriesData = categoriesData;
+            ViewBag.ReservationsData = reservationsData;
+
+            return View();
+        }
+    }
+}
